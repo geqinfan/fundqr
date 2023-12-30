9bbcd0930f8dea7a84f404b3249b081af8d883bd
zhang bob <fangeqin@sina.com>
2023-12-30 10:41:43 +0800

    修改了配置文件格式和referer值

--- dlfilebyurl.py
+++ dlfilebyurl.py
@@ -1,9 +1,9 @@
 import requests
 
 
-def download(url,filename):
+def download(url, filename):
     req = requests.get(url)
-#    filename = url.split('/')[-1]
+    #    filename = url.split('/')[-1]
     if req.status_code != 200:
         print('下载异常')
         return
@@ -18,4 +18,4 @@ def download(url,filename):
 
 if __name__ == '__main__':
     url = 'http://static.cninfo.com.cn/finalpage/2023-01-20/1215717029.PDF'  # xxx为url，根据自己需求输入
-    download(url)
+    download(url, "1215717029.PDF")
--- dlfundqrv2.py
+++ dlfundqrv2.py
@@ -34,27 +34,38 @@ def get_announce_dicts(url, params, headers):
     return announce_dicts
 
 
+"""
+Request URL，服务器端处理客户查询请求的接口
+打开巨潮资讯网，定位到基金页面，打开浏览器的开发者工具（F12），选择NetWork，选择Fetch/XHR，此时在页面输入基金代码，点击查询，此时在devtools中可以看到，返回了一个query，
+在对应的Headers标签页可以查到Request URL
+在referer指向的URL处也可以查到
+具体可参见这篇文章：用python批量获取公募基金季报pdf，https://blog.51cto.com/u_14328065/2849058
+"""
 url = "http://www.cninfo.com.cn/new/hisAnnouncement/query"
 
-configfile='基金代码配置文件.txt'
+configfile = '基金代码配置文件.txt'
 if not os.path.exists(configfile):
     print(f'文件:{configfile} 不存在')
     exit(-1)
 
 with open(configfile) as file_object:
     contents = file_object.read()
-    fundcodes = eval(contents)
+    funds = eval(contents)
 
-    for _, code in fundcodes.items():
+    for item in funds:
         params = {
             'pageNum': 1,
             'pageSize': 30,
-            'stock': code[0],
+            'stock': item['jjdm']+','+item['orgId'],  # referer中stock参数的值
             'isHLtitle': True
         }
 
-        referer = 'http://www.cninfo.com.cn/new/disclosure/stock?stockCode=' + code[0].split(sep=',')[0] + '&orgId=' + \
-                  code[0].split(sep=',')[1]
+        """
+        referer:客户端请求的URL，获取方法是在巨潮资讯网首页顶端输入基金代码点击搜索按钮，在得到的页面中点击最新公告右端的更多>，查看点出的页面地址栏信息
+        """
+        # referer = 'http://www.cninfo.com.cn/new/disclosure/stock?stockCode=' + item['jjdm'] + '&orgId=' + item['orgId']
+        referer = 'http://www.cninfo.com.cn/new/disclosure/'
+        # print('referer:', referer)
 
         headers = {
             'Accept': '*/*',
@@ -63,22 +74,23 @@ with open(configfile) as file_object:
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
         }
 
+        pattern = item['pattern']
+        # print(pattern)
         announce_dicts = get_announce_dicts(url, params, headers)
-
-        for i in range(len(announce_dicts)):
+        dictslen = len(announce_dicts)
+        for i in range(dictslen):
             '''
             print('%2d secCode:%s secName:%s 公告日期：%d 公告标题：%s' % (
                 i + 1, announce_dicts[i]['secCode'], announce_dicts[i]['secName'], announce_dicts[i]['announcementTime'],
                 announce_dicts[i]['announcementTitle']))
             '''
             # if announce_dicts[i]['announcementTitle'].find(code[1]) != -1:
-            pattern = code[1]
+
             title = announce_dicts[i]['announcementTitle']
-            #print(pattern)
-            #print(title)
+            # print(title)
             ret = re.match(pattern, title)
             if ret is None:
-                #print("Error:文件模式匹配失败")
+                # print("Error:文件模式匹配失败")
                 pass
             else:
                 quarter = ret.group(3)
@@ -90,10 +102,10 @@ with open(configfile) as file_object:
                     quarter = '3'
                 if quarter == '四':
                     quarter = '4'
-                #print(quarter)
-                adjustfilename = ret.group(1) + ret.group(2) + '年度第' + quarter + '季度报表.pdf'
+                # print(quarter)
+                adjustfilename = ret.group(1) + ret.group(2) + '年第' + quarter + '季度报告.pdf'
                 pdffileurl = 'http://static.cninfo.com.cn/' + announce_dicts[i]['adjunctUrl']
-
+                # print(pdffileurl)
                 download(pdffileurl, adjustfilename)
                 '''
                 url = 'http://www.cninfo.com.cn/new/announcement/bulletin_detail'
